55f95feefe2db5706307e565cf50fb58a1629bbb
oduma <tavi_duma2002@yahoo.co.uk>
2013-01-15 23:26:53 +0000

    started on the client tool

---
+++ MySynch.ClientConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MySynch.ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Monitoring... (press enter to stop it)");
+            //Start monitoring the queue and respond to a number of situations
+            string sourceRootFolder;
+            FileSystemWatcher fsWatcher= new FileSystemWatcher(sourceRootFolder);
+            fsWatcher.Created += new FileSystemEventHandler(fsWatcher_Created);
+            fsWatcher.Changed += new FileSystemEventHandler(fsWatcher_Changed);
+            fsWatcher.Deleted += new FileSystemEventHandler(fsWatcher_Deleted);
+            fsWatcher.Renamed += new RenamedEventHandler(fsWatcher_Renamed);
+            
+            do
+            {
+                
+            } while (Console.ReadLine()!=string.Empty);
+
+        }
+
+        static void fsWatcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            //queue a delete
+            //queue an insert
+        }
+
+        static void fsWatcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            //queue a delete
+        }
+
+        static void fsWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            //queue an update;
+        }
+
+        static void fsWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            //queue an insert;
+        }
+
+    }
+}
---
+++ MySynch.Core/Interfaces/IChangePublisher.cs
@@ -0,0 +1,13 @@
+﻿using MySynch.Core.DataTypes;
+
+namespace MySynch.Core.Interfaces
+{
+    public interface IChangePublisher
+    {
+        int MaxItemsPerPackage { get; set; }
+
+        void QueueChange(ChangePushItem item);
+
+        ChangePushPackage PublishPackage();
+    }
+}
