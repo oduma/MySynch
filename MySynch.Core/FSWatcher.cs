0fd6a477780fd4b2f9520f2e57ae06659c0502c4
oduma <tavi_duma2002@yahoo.co.uk>
2013-02-18 22:26:21 +0000

    made the fswatcher to trigger only for files

--- MySynch.Core/FSWatcher.cs
+++ MySynch.Core/FSWatcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using MySynch.Common;
 using MySynch.Core.Interfaces;
 
@@ -19,6 +20,8 @@ namespace MySynch.Core
             _changePublisher = changePublisher;
 
             FileSystemWatcher fsWatcher = new FileSystemWatcher(changePublisher.RootFolder);
+            fsWatcher.IncludeSubdirectories = true;
+            
 
             Path = fsWatcher.Path;
 
@@ -33,6 +36,17 @@ namespace MySynch.Core
         private void fsWatcher_Renamed(object sender, RenamedEventArgs e)
         {
             LoggingManager.Debug("A File renamed from " + e.OldFullPath + " to " + e.FullPath);
+            //if it is a directory ignore it
+            if (!File.Exists(e.FullPath))
+                return;
+            //queue an insert;
+            // Wait if file is still open
+            FileInfo fileInfo = new FileInfo(e.FullPath);
+            while (IsFileLocked(fileInfo))
+            {
+                Thread.Sleep(500);
+            }
+
             //queue a delete
             //queue an insert
             _changePublisher.QueueDelete(e.OldFullPath);
@@ -43,6 +57,9 @@ namespace MySynch.Core
         private void fsWatcher_Deleted(object sender, FileSystemEventArgs e)
         {
             LoggingManager.Debug("A file deleted: " + e.FullPath);
+            //if it is a directory ignore it
+            if (!File.Exists(e.FullPath))
+                return;
             //queue a delete
             _changePublisher.QueueDelete(e.FullPath);
         }
@@ -50,6 +67,16 @@ namespace MySynch.Core
         private void fsWatcher_Changed(object sender, FileSystemEventArgs e)
         {
             LoggingManager.Debug("A file changed: " + e.FullPath);
+            //if it is a directory ignore it
+            if (!File.Exists(e.FullPath))
+                return;
+            //queue an insert;
+            // Wait if file is still open
+            FileInfo fileInfo = new FileInfo(e.FullPath);
+            while (IsFileLocked(fileInfo))
+            {
+                Thread.Sleep(500);
+            }
             //queue an update;
             _changePublisher.QueueUpdate(e.FullPath);
         }
@@ -57,8 +84,44 @@ namespace MySynch.Core
         private void fsWatcher_Created(object sender, FileSystemEventArgs e)
         {
             LoggingManager.Debug("A new file created: " + e.FullPath);
+            //if it is a directory ignore it
+            if (!File.Exists(e.FullPath))
+                return;
             //queue an insert;
+            // Wait if file is still open
+            FileInfo fileInfo = new FileInfo(e.FullPath);
+            while (IsFileLocked(fileInfo))
+            {
+                Thread.Sleep(500);
+            }
             _changePublisher.QueueInsert(e.FullPath);
+        }
+
+        static bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open,
+                         FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
         }
 
     }
