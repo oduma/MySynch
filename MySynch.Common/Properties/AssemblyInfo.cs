fae809b2e04cb9476f0727b84090fbc63207c20d
oduma <tavi_duma2002@yahoo.co.uk>
2013-02-20 19:26:24 +0000

    started the versioning for version 1.1

--- MySynch.Common/Properties/AssemblyInfo.cs
+++ MySynch.Common/Properties/AssemblyInfo.cs
@@ -21,16 +21,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("83b61bc5-0f09-48e0-9e1d-303342d86233")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
