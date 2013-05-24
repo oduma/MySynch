75176855cad7f6117f96e665a8a46c53c6ba8708
oduma <tavi_duma2002@yahoo.co.uk>
2013-05-24 18:46:30 +0100

    Preparation for first try to get live

--- MySynch.Services.Installer/buildpackages.bat
+++ MySynch.Services.Installer/buildpackages.bat
@@ -2,16 +2,16 @@ xcopy ..\MySynch.Publisher\Assemblies\*.exe ..\MySynch.Publisher\Exe\*.* /Y
 del ..\MySynch.Publisher\Assemblies\*.exe
 xcopy ..\MySynch.Subscriber\Assemblies\*.exe ..\MySynch.Subscriber\Exe\*.* /Y
 del ..\MySynch.Subscriber\Assemblies\*.exe
-xcopy ..\MySynch.Distributor\Assemblies\*.exe ..\MySynch.Distributor\Exe\*.* /Y
-del ..\MySynch.Distributor\Assemblies\*.exe
+xcopy ..\MySynch.Broker\Assemblies\*.exe ..\MySynch.Broker\Exe\*.* /Y
+del ..\MySynch.Broker\Assemblies\*.exe
 
 xcopy ..\MySynch.Publisher\Assemblies\*.exe.config ..\MySynch.Publisher\Config\*.* /Y
 del ..\MySynch.Publisher\Assemblies\*.exe.config
 xcopy ..\MySynch.Subscriber\Assemblies\*.exe.config ..\MySynch.Subscriber\Config\*.* /Y
 del ..\MySynch.Subscriber\Assemblies\*.exe.config
-xcopy ..\MySynch.Distributor\Assemblies\*.exe.config ..\MySynch.Distributor\Config\*.* /Y
-del ..\MySynch.Distributor\Assemblies\*.exe.config
+xcopy ..\MySynch.Broker\Assemblies\*.exe.config ..\MySynch.Broker\Config\*.* /Y
+del ..\MySynch.Broker\Assemblies\*.exe.config
 
 heat dir ..\MySynch.Publisher\Assemblies -srd -dr PUBLISHER -cg PublisherGroup -var var.PublisherSource -gg -out PublisherGroup.wxs
 heat dir ..\MySynch.Subscriber\Assemblies -srd -dr SUBSCRIBER -cg SubscriberGroup -var var.SubscriberSource -gg -out SubscriberGroup.wxs
-heat dir ..\MySynch.Distributor\Assemblies -srd -dr DISTRIBUTOR -cg DistributorGroup -var var.DistributorSource -gg -out DistributorGroup.wxs
+heat dir ..\MySynch.Broker\Assemblies -srd -dr BROKER -cg BrokerGroup -var var.BrokerSource -gg -out BrokerGroup.wxs
